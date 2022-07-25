a580da76b1646b22af335e28a249fe958def7b40
Jonathan Nguyen <njonathan990@gmail.com>
2022-07-24 20:13:44 -0500

    starting sagas

---
+++ src/components/AddTransaction/AddTransaction.jsx
@@ -0,0 +1,8 @@
+import {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+
+function AddTransaction() {
+  console.log('in addTransaction');
+}
+
+export default AddTransaction;
