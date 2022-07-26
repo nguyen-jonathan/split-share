d95d9be8b3cba26fefd358536b625c54452b3777
Jonathan Nguyen <njonathan990@gmail.com>
2022-07-26 15:50:26 -0500

    moved fetchGroup Saga

---
+++ src/redux/sagas/fetchGroup.saga.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import {takeLatest, put} from 'redux-saga/effects';
+
+function* fetchGroupSaga() {
+  yield takeLatest('GET_GROUP_SAGA', getGroup);
+}
+
+function* fetchGroup() {
+  try {
+    const res = yield axios.get(`/api/group/fetchGroup`);
+    yield put({type: 'GET_GROUP', payload: res.data});
+    console.log(res.data);
+  } catch (err) {
+    alert('error getting Group');
+  }
+}
+
+export default fetchGroupSaga;
