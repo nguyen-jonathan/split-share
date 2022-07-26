4ffc65874f8a40737c0d503a133c2cc72b364859
Jonathan Nguyen <njonathan990@gmail.com>
2022-07-26 15:52:49 -0500

    moved newGroup reducer

---
+++ src/redux/reducers/newGroup.reducer.js
@@ -0,0 +1,9 @@
+// reducer to store new group name
+const newGroup = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_NEW_GROUP':
+      return action.payload;
+    default:
+      return state;
+  }
+};
