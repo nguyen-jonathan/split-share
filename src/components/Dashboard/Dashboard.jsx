2d8729b9c7b630fcd1b3415f1b38b9b7970b8d49
Jonathan Nguyen <njonathan990@gmail.com>
2022-07-27 02:40:48 -0500

    adding button click on dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -8,12 +8,24 @@ import Container from 'react-bootstrap/Container';
 // or even care what the redux state is
 
 function Dashboard() {
+  const handleClickExpense = () => {
+    alert('adding expense');
+  };
+  const handleClickGroup = () => {
+    alert('adding group');
+  };
+
   return (
     <Container className="my-4">
       <Stack direction="horizontal" gap="2" className="mb-4">
         <h2 className="me-auto">Dashboard</h2>
         <h4 className="mx-auto">Total Balance</h4>
-        <Button variant="primary">Add Expense</Button>
+        <Button variant="primary" onClick={handleClickExpense}>
+          Add Expense
+        </Button>
+        <Button variant="primary" onClick={handleClickGroup}>
+          Add Group
+        </Button>
       </Stack>
       {/* <div
         style={{
